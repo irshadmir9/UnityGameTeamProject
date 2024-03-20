5ad5c5480ede2b0380cd4348a2a2c089a2ccb0c6
Adarsh-Kumar24 <aadiawesome1@gmail.com>
2024-03-20 10:59:59 -0500

    3/20 Movement

---
+++ Escape Chernobyl/Assets/PlayerMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveInput, 0, 0) * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb2d.velocity.y) < 0.001)
+        {
+            rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}

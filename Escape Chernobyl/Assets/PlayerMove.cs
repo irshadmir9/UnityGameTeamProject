c99fe32814194733ce13c9c756f1c8b3e493660f
irshadmir9 <spideeboy9@gmail.com>
2024-04-02 11:18:01 -0500

    npc

--- Escape Chernobyl/Assets/PlayerMove.cs
+++ Escape Chernobyl/Assets/PlayerMove.cs
@@ -12,7 +12,12 @@ public class PlayerMove : MonoBehaviour
 
     public Animator animator;
     public bool isJumping = false;
- 
+
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +27,17 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
         float moveInput = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(moveInput, 0, 0) * moveSpeed * Time.deltaTime;
         
@@ -58,6 +74,16 @@ public class PlayerMove : MonoBehaviour
         if(moveInput > 0)
         {
             transform.localScale = new Vector3(4, 4, 4);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("hi");
+            RaycastHit2D hit = Physics2D.Raycast(rb2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+            }
         }
     }
 }

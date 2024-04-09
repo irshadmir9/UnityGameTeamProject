d139443b07ed25ba1f2b99041e9eaea56e72041a
irshadmir9 <spideeboy9@gmail.com>
2024-04-09 11:16:43 -0500

    dialogue

---
+++ Escape Chernobyl/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject window;
+    public List<string> dialogues;
+    private int index;
+    private int charIndex;
+
+    private bool started;
+    public TMP_Text dialogueText;
+
+    private void ToggleWindow(bool show)
+    {
+        window.SetActive(show);
+    }
+    public void StartDialogue()
+    {
+        if(started)
+        {
+            return;
+        }
+        started = true;
+
+        ToggleWindow(true);
+
+        GetDialogue(0);
+    }
+
+    private void GetDialogue(int i)
+    {
+        index = i;
+        charIndex = 0;
+        dialogueText.text = string.Empty;
+        StartCoroutine (Writing()); 
+    }
+
+    public void EndDialogue()
+    {
+
+    }
+
+    IEnumerator Writing()
+    {
+        string currentDialogue = dialogues[index];
+        dialogueText.text += currentDialogue[charIndex];
+    }
+}
